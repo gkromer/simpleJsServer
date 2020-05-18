c5263e771b5d49653f30d2f583208287ba4c2d01
Georg Kromer <georgkromer@protonmail.com>
2020-05-18 18:53:36 +0200

    removed local address heroku doesnt use 127.0.0.1

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-type', 'text/plain');
-    res.end('helloklk');
+    res.end('Welcome to my JS Server');
 });
 
-server.listen(PORT, hostname, () => {
+server.listen(PORT, () => {
     console.log('Server started on port '+ PORT);
 })
